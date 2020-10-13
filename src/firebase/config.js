6091d404902591516fabd99fccc859e72bcba5d0
sanketh <jsanketh1799@gmail.com>
2020-10-14 00:49:12 +0530

    firebase set up

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBsMSX_yBK4XlYBlwaHx4vHOAgP9FAWm_M",
+    authDomain: "hunt-eaa45.firebaseapp.com",
+    databaseURL: "https://hunt-eaa45.firebaseio.com",
+    projectId: "hunt-eaa45",
+    storageBucket: "hunt-eaa45.appspot.com",
+    messagingSenderId: "528195094456",
+    appId: "1:528195094456:web:62d1357f7d0cfbe495b14d",
+    measurementId: "G-F08J40M2WB"
+  };
+
+  export default firebaseConfig;
