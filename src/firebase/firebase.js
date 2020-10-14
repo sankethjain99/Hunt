e82df2e9ff6f0c3aff63a0ae09ba16164c566be7
sanketh <jsanketh1799@gmail.com>
2020-10-14 20:44:02 +0530

    authentication done

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,37 +1,39 @@
-import app from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-import firebaseConfig from './config';
 
-class Firebase{
-    constructor(){
-        app.initializeApp(firebaseConfig);
-        this.app=app;
-        this.auth=app.auth();
-        this.db=app.firestore();
-    }
+import app from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
-    async register(name,email,password){
-        const newUser = await this.auth.createUserWithEmailAndPassword(
-            email,
-            password
-        );
-        return newUser.user.updateProfile({
-            displayName: name
-        })
-    }
+import firebaseConfig from "./config";
 
-    login(email,password){
-        return this.auth.signInWithEmailAndPassword(email,password);
-    }
+class Firebase {
+  constructor() {
+    app.initializeApp(firebaseConfig);
+    this.app = app;
+    this.auth = app.auth();
+    this.db = app.firestore();
+  }
 
-    logout(){
-        return this.auth.signOut();
-    }
+  async register(name, email, password) {
+    const newUser = await this.auth.createUserWithEmailAndPassword(
+      email,
+      password
+    );
+    return newUser.user.updateProfile({
+      displayName: name
+    });
+  }
 
-    resetPassword(email){
-        return this.auth.sendPasswordResetEmail(email);
-    }
+  login(email, password) {
+    return this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  logout() {
+    return this.auth.signOut();
+  }
+
+  resetPassword(email) {
+    return this.auth.sendPasswordResetEmail(email);
+  }
 }
 
 const firebase = new Firebase();
